4553e03d481c20430b5487b94666e2bcd94be16e
Jeyfred <jeyfredc@gmail.com>
2021-03-11 11:45:16 -0500

    Sidebar

---
+++ 07-journal-app/src/components/journal/Sidebar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export const Sidebar = () => {
+    return (
+       <aside className="journal__sidebar">
+           
+           <div className="journal_sidebar-navbar">
+               <h3 className="mt-5">
+
+               <i className="far fa-moon" />
+                <span> Jeyfred</span>
+               </h3>
+
+               <button className="btn">
+                   Logout
+               </button>
+
+           </div>
+
+           <div className="journal__new-entry">
+               <i className="far fa-calendar-plus fa-5x" />
+                <p className="mt-5">
+                    New entry
+                </p>
+           </div>
+       </aside>
+    )
+}
